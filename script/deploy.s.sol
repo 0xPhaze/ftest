8fcab132e5a4ad1bd4a7144be0af58d149555f87
0xphaze <0xphaze@gmail.com>
2022-09-25 00:17:03 +0200

    unlinked

--- script/deploy.s.sol
+++ script/deploy.s.sol
@@ -3,10 +3,9 @@ pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
 
-import "../src/Contract.sol";
+contract Contract {}
 
 /* 
-# Anvil
 forge script deploy --rpc-url http://127.0.0.1:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 -vvvv --ffi --broadcast 
 
 If running the script ^ with `--broadcast` after `forge clean` (without running it without `--broadcast` before)
